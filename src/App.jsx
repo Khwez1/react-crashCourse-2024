0627cb5192fbd6c76e9132fb3f30acfe9c7bfe7b
Sihle Mbobo <khwezimbobo@gmail.com>
2024-05-05 02:53:16 +0200

    react spinner

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import Joblistings from './components/JobsListings'
 import ViewAllJobs from './components/ViewAllJobs'
 import MainLayout from './Layouts/MainLayout'
 import HomePage from './Pages/HomePage'
+import JobsPage from './Pages/JobsPage'
+import NotFoundPage from './Pages/NotFound'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
-    <Route index element={<HomePage />} />
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
 );
